e91467e10dccc4f2c98b58be1be86fb44a15b28d
StepanTita <stepan.tytarenko@nure.ua>
2023-11-01 11:38:09 -0400

    update inter-space loss

--- space_model/loss.py
+++ space_model/loss.py
@@ -9,7 +9,7 @@ def inter_space_loss(concept_spaces: List[torch.Tensor], eps=1e-9):
     loss = 0.0
     for k in range(len(concept_spaces)):
         for l in range(k + 1, len(concept_spaces)):
-            loss += (1.0 / (1.0 - F.tanh(concept_spaces[k].mean(dim=1) @ concept_spaces[l].mean(dim=1).T) + eps)).sum(1)
+            loss += (1.0 / (1.0 - F.tanh(concept_spaces[k].mean(dim=0).mean(dim=0) @ concept_spaces[l].mean(dim=0).mean(dim=0)) + eps))
     return loss.mean(0)
 
 
