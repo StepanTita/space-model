a948e545a15f837da51a141ed194e5b0f580bec1
StepanTita <stepun.tita@gmail.com>
2024-07-01 18:54:17 -0400

    add Concept Layer and return Space Model

--- space_model/model.py
+++ space_model/model.py
@@ -14,7 +14,7 @@ class SpaceModelOutput:
         return f'{self.logits=},{self.concept_spaces=},{self.raw_concept_spaces=}'
 
 
-class SpaceLayer(torch.nn.Module):
+class SpaceModel(torch.nn.Module):
     def __init__(self, n_embed=3, n_latent=3, n_concept_spaces=2, output_concept_spaces=True):
         super().__init__()
 
@@ -41,11 +41,32 @@ class SpaceLayer(torch.nn.Module):
         return SpaceModelOutput(concept_logits, concept_spaces, raw_projected_x)
 
 
+class SpaceLayer(torch.nn.Module):
+    def __init__(self, n_embed=3, n_latent=3, n_concept_spaces=2):
+        super().__init__()
+
+        # project embedding space (B, max_seq_len, n_embed) -> to the concept space (B, n_embed, n_latent) = (B, max_seq_len, n_latent)
+        self.concept_spaces = torch.nn.ModuleList([
+            # unlike in the original paper, here we use a combination of Dense layer + tanh instead of cosine similarity
+            torch.nn.Linear(in_features=n_embed, out_features=n_latent, bias=False) for _ in range(n_concept_spaces)
+        ])
+
+    def forward(self, x):
+        raw_projected_x = [space(x) for space in self.concept_spaces]  # (n_concept_spaces, B, max_seq_len, n_latent)
+        projected_x = [F.tanh(x) for x in raw_projected_x]  # (n_concept_spaces, B, max_seq_len, n_latent)
+
+        avg_concept_attention = [x.mean(1) for x in projected_x]  # (n_concept_spaces, B, n_latent)
+
+        concept_logits = torch.cat(avg_concept_attention, dim=-1)  # (B, n_concept_spaces * n_latent)
+
+        return concept_logits
+
+
 class SpaceModelForClassification(torch.nn.Module):
     def __init__(self, n_embed=3, n_latent=3, n_concept_spaces=2):
         super().__init__()
 
-        self.space_model = SpaceLayer(n_embed, n_latent, n_concept_spaces, output_concept_spaces=True)
+        self.space_model = SpaceModel(n_embed, n_latent, n_concept_spaces, output_concept_spaces=True)
 
         # number of target classes is equal to the number of concepts
         self.concept_classifier = torch.nn.Linear(in_features=n_concept_spaces * n_latent,
@@ -80,7 +101,7 @@ class SpaceModelForSequenceClassification(torch.nn.Module):
 
         self.base_model = base_model
 
-        self.space_model = SpaceLayer(n_embed, n_latent, n_concept_spaces, output_concept_spaces=True)
+        self.space_model = SpaceModel(n_embed, n_latent, n_concept_spaces, output_concept_spaces=True)
 
         self.classifier = torch.nn.Linear(n_concept_spaces * n_latent, n_concept_spaces)
 
@@ -144,7 +165,7 @@ class SpaceModelForMultiLabelClassification(torch.nn.Module):
             for p in base_model.parameters():
                 p.requires_grad_(False)
 
-        self.space_model = SpaceLayer(n_embed, n_latent, n_concept_spaces, output_concept_spaces=True)
+        self.space_model = SpaceModel(n_embed, n_latent, n_concept_spaces, output_concept_spaces=True)
 
         self.classifier = torch.nn.Linear(n_concept_spaces * n_latent, n_concept_spaces)
 
